67ddc89ace23dacc22f8180fd644cb537ed9378b
fishsquare69ok <qafish01@gmail.com>
2024-09-14 14:32:26 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/style.css',   // 假設你有 CSS 文件
-  '/script.js',   // 假設你有 JavaScript 文件
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',   // 假設你有 CSS 文件
+  './script.js',   // 假設你有 JavaScript 文件
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
